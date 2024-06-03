44edfaebb1d87d3eaeff911bf9a9f6ba32497532
vegtb <bas.van.der.vegt@hva.nl>
2024-06-03 19:39:48 +0200

    Add more files for start of project

--- data/get_data.py
+++ data/get_data.py
@@ -2,3 +2,14 @@
 # A live connection to filter data during the process of running the script should be implemented.
 # Meaning during the process of filtering and plotting new data should be
 # transferred from the database to the python script / dashboard
+
+# Get dataframe from a .csv file
+def get_dataframe_from_csv(filepath):
+    df = "csv"
+    return df
+
+
+# Get dataframe from MongoDB
+def get_dataframe_from_mongodb(database, query):
+    df = "mongo"
+    return df
---
+++ data/store_data.py
@@ -0,0 +1,3 @@
+# Store data in mongodb
+def store_dataframe_in_mongodb(dataframe):
+    return ""
